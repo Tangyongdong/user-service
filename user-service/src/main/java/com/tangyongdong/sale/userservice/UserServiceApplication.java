e21261dd243c5630dca41a1763c23a53c40bca5e
tangyongdong <Tyd123zxz>
2018-05-04 18:38:33 +0800

    Initial commit

---
+++ user-service/src/main/java/com/tangyongdong/sale/userservice/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tangyongdong.sale.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+}
